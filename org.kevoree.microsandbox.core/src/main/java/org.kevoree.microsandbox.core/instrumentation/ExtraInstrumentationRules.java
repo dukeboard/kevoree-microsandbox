9000e5dea548a1630a6f2ca41965e61919f4b10b
Inti Glez <inti.gonzalez_herrera@irisa.fr>
2013-07-02 18:55:43 +0200

    Avoiding the instrumentation of RT java in raspberry because everything crash if I do it

--- org.kevoree.microsandbox.core/src/main/java/org.kevoree.microsandbox.core/instrumentation/ExtraInstrumentationRules.java
+++ org.kevoree.microsandbox.core/src/main/java/org.kevoree.microsandbox.core/instrumentation/ExtraInstrumentationRules.java
@@ -34,7 +34,32 @@ public class ExtraInstrumentationRules {
 
     public static boolean isMemoryAccountable(String className) {
 
-        return !(className.startsWith("java/lang/"))
-                && !(className.startsWith("java/security"));
+        if (className.startsWith("org/kevoree/microsandbox/samples"))
+            return true;
+
+        if (className.startsWith("java")
+                || className.startsWith("sun")
+                || className.startsWith("com/sun/")
+                || className.startsWith("com/oracle/")
+                || className.startsWith("org/xml/sax")
+                || className.startsWith("org/w3c/dom")
+                || className.startsWith("org/jcp/")
+                || className.startsWith("org/omg/")
+                || className.startsWith("org/ietf/"))
+            return false;
+        if (className.startsWith("org/kevoree/monitoring")
+                || className.startsWith("org/kevoree/microsandbox")
+                || className.startsWith("org/resourceaccounting/")
+                || className.startsWith("org/objectweb/asm")
+                || className.startsWith("org/jacoco/")
+                )
+            return false;
+        return true;
     }
+
+//    public static boolean isMemoryAccountable(String className) {
+//
+//        return !(className.startsWith("java/lang/"))
+//                && !(className.startsWith("java/security"));
+//    }
 }
