08342c7286cf67ceb9737e716ddb1527dc669a63
Inti Glez <inti.gonzalez_herrera@irisa.fr>
2013-07-02 18:58:26 +0200

    the agent needs more time to activate the proxy class in raspberry. Probably because the device is slower than a PC

--- ResourceMonitorJavaAgent/src/main/java/org/resourceaccounting/binderinjector/ResourceCounterAgent.java
+++ ResourceMonitorJavaAgent/src/main/java/org/resourceaccounting/binderinjector/ResourceCounterAgent.java
@@ -42,7 +42,7 @@ public class ResourceCounterAgent {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(100);
+                    Thread.sleep(2000);
                     globalInst.retransformClasses(Integer.class);
                 }
                 catch (UnmodifiableClassException e) { }
