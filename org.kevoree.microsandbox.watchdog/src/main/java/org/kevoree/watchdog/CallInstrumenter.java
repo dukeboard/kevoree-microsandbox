53476c2cfb8665cac0d38649d4009c856f6ea39d
Francois Fouquet <fouquet.f@gmail.com>
2013-07-04 12:25:35 +0200

    cleanup

--- org.kevoree.microsandbox.watchdog/src/main/java/org/kevoree/watchdog/CallInstrumenter.java
+++ org.kevoree.microsandbox.watchdog/src/main/java/org/kevoree/watchdog/CallInstrumenter.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Arrays;
@@ -16,25 +17,30 @@ import java.util.Arrays;
  */
 public class CallInstrumenter {
 
-    public void createRTIntrumentedJar(File target) {
+    public static void main(String[] args){
+        createRTIntrumentedJar(null);
+    }
+
+    public static void createRTIntrumentedJar(File target) {
 
         try {
-            File instrumenterJar = copyToTemp(this.getClass().getClassLoader().getResourceAsStream("jarInstrumenter.jar"));
+            File instrumenterJar = copyToTemp(CallInstrumenter.class.getClassLoader().getResourceAsStream("jarInstrumenter.jar"));
             URL[] urls = new URL[1];
-            urls[0] = new URL(instrumenterJar.getAbsolutePath());
+            urls[0] = new URL("file:///"+instrumenterJar.getAbsolutePath());
             URLClassLoader cl = new URLClassLoader(urls);
             Class mainClazz = cl.loadClass("org.kevoree.microsandbox.jarInstrument.Main");
             String rtJar = "/Library/Java/Home/bundle/Home/bundle/Classes/classes.jar";
-
-
-
+            Method mainM = mainClazz.getMethod("main", String[].class);
+            String[] args = new String[1];
+            args[0] = rtJar;
+            mainM.invoke(null, (Object) args);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public File copyToTemp(InputStream is) throws IOException {
+    public static File copyToTemp(InputStream is) throws IOException {
         File tempFile = File.createTempFile("jarInstrumentJar",".jar");
         FileOutputStream fos = new FileOutputStream(tempFile);
         byte data[] = new byte[1024];
