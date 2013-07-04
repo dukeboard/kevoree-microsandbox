3d28b4401979864009c24fe4c08b41e6032e83ef
Erwan Daubert <erwan.daubert@gmail.com>
2013-07-04 11:06:03 +0200

    initial commit for unit test

---
+++ org.kevoree.microsandbox.test/src/test/java/org/kevoree/microsandbox/test/MemoryContractedTester.java
@@ -0,0 +1,12 @@
+package org.kevoree.microsandbox.test;
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 04/07/13
+ * Time: 10:27
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+public class MemoryContractedTester {
+}
