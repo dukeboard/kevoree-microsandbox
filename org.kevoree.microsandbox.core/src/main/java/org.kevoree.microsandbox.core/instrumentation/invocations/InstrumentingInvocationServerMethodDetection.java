629f3008aefa6249436093dcabd4bdd075402750
Inti Glez <inti.gonzalez_herrera@irisa.fr>
2013-07-05 19:29:39 +0200

    Detecting boths, requests for invocations and handling of invocations. Upps, I forgot this file

---
+++ org.kevoree.microsandbox.core/src/main/java/org.kevoree.microsandbox.core/instrumentation/invocations/InstrumentingInvocationServerMethodDetection.java
@@ -0,0 +1,11 @@
+package org.kevoree.microsandbox.core.instrumentation.invocations;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: inti
+ * Date: 7/5/13
+ * Time: 6:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class InstrumentingInvocationServerMethodDetection {
+}
