35e66b22a5743aadd1220b8bc38ee81523466bad
Francois Fouquet <fouquet.f@gmail.com>
2013-07-08 10:57:16 +0200

    k

--- org.kevoree.microsandbox.monitoring/src/main/java/org/kevoree/monitoring/models/impl/FindAlternativeTypeDefinition.kt
+++ org.kevoree.microsandbox.monitoring/src/main/java/org/kevoree/monitoring/models/impl/FindAlternativeTypeDefinition.kt
@@ -1,9 +1,31 @@
 package org.kevoree.monitoring.models.impl
 
+import org.kevoree.ContainerRoot
+import org.kevoree.TypeDefinition
+import java.util.HashSet
+
 /**
  * Created with IntelliJ IDEA.
  * User: duke
  * Date: 08/07/13
  * Time: 10:44
- * To change this template use File | Settings | File Templates.
  */
+
+public object FindAlternativeTypeDefinition {
+
+    public fun findCompatible(origin: TypeDefinition, models: List<ContainerRoot>): List<TypeDefinition> {
+        val typeDef = HashSet<TypeDefinition>()
+        val originPath = origin.path()
+        if(originPath != null){
+            for(model in models){
+                val similarTypeDef = model.findByPath(originPath)
+                if(!origin.deepModelEquals(similarTypeDef)){
+                    //TODO check if contract is compatible
+                    typeDef.add(similarTypeDef as TypeDefinition)
+                }
+            }
+        }
+        return typeDef.toList()
+    }
+
+}
