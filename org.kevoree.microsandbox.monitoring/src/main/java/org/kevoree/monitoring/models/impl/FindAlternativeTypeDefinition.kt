530d20d44a538712b2c388e1772e24887f9a6718
Francois Fouquet <fouquet.f@gmail.com>
2013-07-08 10:56:39 +0200

    init find alternative type definition

---
+++ org.kevoree.microsandbox.monitoring/src/main/java/org/kevoree/monitoring/models/impl/FindAlternativeTypeDefinition.kt
@@ -0,0 +1,9 @@
+package org.kevoree.monitoring.models.impl
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: duke
+ * Date: 08/07/13
+ * Time: 10:44
+ * To change this template use File | Settings | File Templates.
+ */
