8afbc7bdbf96b2f43e33d41a70fc5bfd82dd3388
Inti Glez <inti.gonzalez_herrera@irisa.fr>
2013-09-05 16:13:41 +0200

    Component to execute dacapo benchmark

---
+++ org.kevoree.microsandbox.samples/src/main/java/org/kevoree/microsandbox/samples/benchmark/dacapo/RunningDacapoComponent.java
@@ -0,0 +1,11 @@
+package org.kevoree.microsandbox.samples.benchmark.dacapo;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: inti
+ * Date: 9/5/13
+ * Time: 2:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RunningDacapoComponent {
+}
