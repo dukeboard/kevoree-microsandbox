587f38c04f6a956015dea121f2c6ee9e83a662ef
Erwan Daubert <erwan.daubert@gmail.com>
2013-09-30 14:07:10 +0200

    update microsandbox api

---
+++ org.kevoree.microsandbox.api/src/main/java/org/kevoree/microsandbox/api/contract/ContractedComponentHelper.kt
@@ -0,0 +1,39 @@
+package org.kevoree.microsandbox.api.contract
+
+import org.kevoree.annotation.DictionaryType
+import org.kevoree.TypeDefinition
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 26/09/13
+ * Time: 17:47
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+object ContractedComponentHelper {
+    fun isContractedComponent(typeDefinition: TypeDefinition): Boolean {
+        val isFullContracted = javaClass<FullContracted>().getAnnotation(javaClass<DictionaryType>())?.value()?.toList()?.all {
+            dictionaryAttribute ->
+            typeDefinition.getDictionaryType()?.getAttributes()?.filter{ attribute -> attribute.getName().equals(dictionaryAttribute.name()) }?.size() == 1
+        }
+        val isMemoryContracted = javaClass<MemoryContracted>().getAnnotation(javaClass<DictionaryType>())?.value()?.toList()?.all {
+            dictionaryAttribute ->
+            typeDefinition.getDictionaryType()?.getAttributes()?.filter{ attribute -> attribute.getName().equals(dictionaryAttribute.name()) }?.size() == 1
+        }
+        val isCPUContracted = javaClass<CPUContracted>().getAnnotation(javaClass<DictionaryType>())?.value()?.toList()?.all {
+            dictionaryAttribute ->
+            typeDefinition.getDictionaryType()?.getAttributes()?.filter{ attribute -> attribute.getName().equals(dictionaryAttribute.name()) }?.size() == 1
+        }
+        val isNetworkContracted = javaClass<NetworkContracted>().getAnnotation(javaClass<DictionaryType>())?.value()?.toList()?.all {
+            dictionaryAttribute ->
+            typeDefinition.getDictionaryType()?.getAttributes()?.filter{ attribute -> attribute.getName().equals(dictionaryAttribute.name()) }?.size() == 1
+        }
+        val isThroughputContracted = javaClass<ThroughputContracted>().getAnnotation(javaClass<DictionaryType>())?.value()?.toList()?.all {
+            dictionaryAttribute ->
+            typeDefinition.getDictionaryType()?.getAttributes()?.filter{ attribute -> attribute.getName().equals(dictionaryAttribute.name()) }?.size() == 1
+        }
+
+        return (isFullContracted != null && isFullContracted!!) || (isMemoryContracted != null && isMemoryContracted!!) || (isCPUContracted != null && isCPUContracted!!) || (isNetworkContracted != null && isNetworkContracted!!) || (isThroughputContracted != null && isThroughputContracted!!)
+    }
+}
---
+++ org.kevoree.microsandbox.api/src/main/java/org/kevoree/microsandbox/api/heuristic/RankingHeuristicComponent.java
@@ -0,0 +1,29 @@
+package org.kevoree.microsandbox.api.heuristic;
+
+import org.kevoree.ComponentInstance;
+
+/**
+ * User: Erwan Daubert - erwan.daubert@gmail.com
+ * Date: 26/09/13
+ * Time: 14:34
+ *
+ * @author Erwan Daubert
+ * @version 1.0
+ */
+public interface RankingHeuristicComponent {
+    /**
+     * calculate the order of the components that must be monitored
+     * First element in the list is the first component to monitor.
+     * @return a @{List} of @{ComponentInstance}.
+     */
+    // use of ComponentInstance instead of List<ComponentInstance> to by pass a bug on Kevoree (version 2.0.12)
+    ComponentInstance[] getRankingOrder(String nodeName);
+
+    /**
+     * allow to send some information from monitoring core to rankingHeuristic
+     * @param name the name of the "event"
+     * @param value the value of the "event"
+     */
+    // FIXME this method must be remove and a better solution must be fine but we don't have time currently
+    void triggerMonitoringEvent(String operation, String name, String instancePath, Long value);
+}
