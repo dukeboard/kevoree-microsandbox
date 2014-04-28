79d70b49dd1c773fbd1a33355ab67490e086ab5a
Inti Gonzalez-Herrera <inti.glez@gmail.com>
2014-04-28 16:20:49 +0200

    updating the downloader

--- org.kevoree.microsandbox.watchdog/run.sh
+++ org.kevoree.microsandbox.watchdog/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Dproject.version=1.0-SNAPSHOT -Dkevoree.corelibrary.version=2.0.5 -Dnode.name=$2  -jar target/org.kevoree.microsandbox.watchdog-1.0-SNAPSHOT.jar 2.0.12 $1
+java -Dproject.version=1.1 -Dkevoree.corelibrary.version=2.0.5 -Dnode.name=$2  -jar target/org.kevoree.microsandbox.watchdog-1.1.jar 2.0.12 $1
