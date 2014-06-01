74762eb308a25c4f2bc99fec09a4a0c830a2f579
Inti Gonzalez-Herrera <inti.glez@gmail.com>
2014-06-01 21:15:16 +0200

    updating version

--- org.kevoree.microsandbox.watchdog/run.sh
+++ org.kevoree.microsandbox.watchdog/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -Dproject.version=1.1 -Dkevoree.corelibrary.version=3.5.2 -Dnode.name=$2  -jar target/org.kevoree.microsandbox.watchdog-1.1.jar 3.5.4 $1
+java -Dproject.version=1.1 -Dkevoree.corelibrary.version=3.5.3 -Dnode.name=$2  -jar target/org.kevoree.microsandbox.watchdog-1.1.jar 3.5.6 $1
--- org.kevoree.microsandbox.watchdog/run2.sh
+++ org.kevoree.microsandbox.watchdog/run2.sh
@@ -12,4 +12,4 @@ if [ $# -eq 4 ]; then
 	log=$4	
 fi
 
-java -Dnode.bootstrap=$1 -Dproject.version=1.1 -Duse.criu=${criu} -Dexperiment.log=${log} -Dkevoree.corelibrary.version=2.0.5 -Dnode.name=$2 -Dinterproccess.lock.library=/home/inti/programs/kevoree-microsandbox/org.ipc.memory/libInterprocessLock.so -Dthread.control.library=/home/inti/programs/kevoree-microsandbox/org.kevoree.microsandbox.cgroupNode/libCPUThreadControl.so -Xbootclasspath/p:/tmp/ext-rt.jar:/tmp/shared-res-1.1.jar -javaagent:/tmp/ext-agent1.1.jar -jar /tmp/kevoree-runtime-2.0.12.jar
+java -Dnode.bootstrap=$1 -Dproject.version=1.1 -Duse.criu=${criu} -Dexperiment.log=${log} -Dkevoree.corelibrary.version=3.5.3 -Dnode.name=$2 -Dinterproccess.lock.library=/home/inti/programs/kevoree-microsandbox/org.ipc.memory/libInterprocessLock.so -Dthread.control.library=/home/inti/programs/kevoree-microsandbox/org.kevoree.microsandbox.cgroupNode/libCPUThreadControl.so -Xbootclasspath/p:/tmp/ext-rt.jar:/tmp/shared-res-1.1.jar -javaagent:/tmp/ext-agent1.1.jar -jar /tmp/kevoree-runtime-3.5.6.jar
