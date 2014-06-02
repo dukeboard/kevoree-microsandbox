f0f0b057fcc09a9eca0d7e19f5c673e682d4516e
Inti Gonzalez-Herrera <inti.glez@gmail.com>
2014-06-02 11:15:15 +0200

    checking some errors

--- org.kevoree.microsandbox.cgroupNode/src/main/image-creation/createClonesForExperiments.sh
+++ org.kevoree.microsandbox.cgroupNode/src/main/image-creation/createClonesForExperiments.sh
@@ -8,6 +8,47 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
+CRIU_APP='/home/inti/Downloads/criu-1.2/criu'
+kevscript=/home/inti/programs/kevoree-microsandbox/org.kevoree.microsandbox.cgroupNode/src/main/kevs/criu-helper/runningInClones.kevs
+thread_library=/home/inti/programs/kevoree-microsandbox/org.kevoree.microsandbox.cgroupNode/libCPUThreadControl.so
+
+#checking a bunch of possible errors
+if [ ! -f ${CRIU_APP} ]; then
+    echo "CRIU is not located at ${CRIU_APP}"
+    echo "It should be if you want to execute this script!!!"
+    exit 1
+fi
+if [ ! -f ${kevscript} ]; then
+    echo "The script runningInClones cannot be found at ${kevscript}"
+    echo "It should be if you want to execute this script!!!"
+    exit 1
+fi
+if [ ! -f ${thread_library} ]; then
+    echo "The thread_library cannot be found at ${thread_library}"
+    echo "It should be if you want to execute this script!!!"
+    exit 1
+fi
+if [ ! -f /tmp/ext-rt.jar ]; then
+    echo "The file ext-rt.jar cannot be found at /tmp"
+    echo "It should be if you want to execute this script!!!"
+    exit 1
+fi
+if [ ! -f /tmp/shared-res-1.1.jar ]; then
+    echo "The file shared-res-1.1.jar cannot be found at /tmp"
+    echo "It should be if you want to execute this script!!!"
+    exit 1
+fi
+if [ ! -f /tmp/ext-agent1.1.jar ]; then
+    echo "The file ext-agent1.1.jar cannot be found at /tmp"
+    echo "It should be if you want to execute this script!!!"
+    exit 1
+fi
+if [ ! -f /tmp/kevoree-runtime-3.5.6.jar ]; then
+    echo "The file kevoree-runtime-3.5.6.jar cannot be found at /tmp"
+    echo "It should be if you want to execute this script!!!"
+    exit 1
+fi
+
 if [ ! -d $1 ]; then
 	mkdir $1
 fi
