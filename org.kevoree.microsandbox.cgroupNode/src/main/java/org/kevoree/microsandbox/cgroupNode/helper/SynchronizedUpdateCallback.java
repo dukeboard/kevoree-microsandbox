3aae3ce48e9905b05117b0579449f3d322dcaac1
Inti Gonzalez-Herrera <inti.glez@gmail.com>
2014-06-03 14:07:41 +0200

    removing bunch of file I wrongly included in the repository

---
+++ org.kevoree.microsandbox.cgroupNode/src/main/java/org/kevoree/microsandbox/cgroupNode/helper/SynchronizedUpdateCallback.java
@@ -0,0 +1,7 @@
+package org.kevoree.microsandbox.cgroupNode.helper;
+
+/**
+ * Created by inti on 03/06/14.
+ */
+public class SynchronizedUpdateCallback {
+}
--- org.kevoree.microsandbox.watchdog/scratch/DataParserTokenTypes.java
+++
@@ -1,8 +0,0 @@
-// $ANTLR 2.7.2: "data.g" -> "DataLexer.java"$
-
-public interface DataParserTokenTypes {
-	int EOF = 1;
-	int NULL_TREE_LOOKAHEAD = 3;
-	int SHORT = 4;
-	int STRING = 5;
-}
