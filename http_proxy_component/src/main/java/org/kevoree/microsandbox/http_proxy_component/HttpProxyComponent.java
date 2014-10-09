16ab5e30d90000e84202c96bc0d360287432026e
Inti Gonzalez-Herrera <inti.glez@gmail.com>
2014-10-09 21:11:13 +0200

    add module to execute MDMS iwth many servers on Kevoree and a load balancer

--- http_proxy_component/src/main/java/org/kevoree/microsandbox/http_proxy_component/HttpProxyComponent.java
+++ http_proxy_component/src/main/java/org/kevoree/microsandbox/http_proxy_component/HttpProxyComponent.java
@@ -1,21 +1,42 @@
 package org.kevoree.microsandbox.http_proxy_component;
 
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.http.HttpObject;
-import io.netty.handler.codec.http.HttpRequest;
-import io.netty.handler.codec.http.HttpResponse;
+import io.netty.handler.codec.http.*;
 import org.kevoree.annotation.*;
+import org.kevoree.log.Log;
 import org.littleshoot.proxy.HttpFilters;
 import org.littleshoot.proxy.HttpFiltersAdapter;
 import org.littleshoot.proxy.HttpFiltersSourceAdapter;
 import org.littleshoot.proxy.HttpProxyServer;
 import org.littleshoot.proxy.impl.DefaultHttpProxyServer;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @ComponentType
 public class HttpProxyComponent {
 
-//    @Param(defaultValue = "true")
-//    boolean log;
+    @Param(defaultValue = "true")
+    boolean log;
+
+    @Param(optional = false)
+    int port;
+
+    private List<Integer> ports = new ArrayList<Integer>();
+
+    int currentPort = 0;
+
+    private synchronized int increaseAndGet() {
+        int tmp = currentPort;
+        currentPort ++;
+        if (currentPort == ports.size())
+            currentPort = 0;
+        return tmp;
+    }
 
     @KevoreeInject
     org.kevoree.api.Context context;
@@ -23,52 +44,73 @@ public class HttpProxyComponent {
 //    @Output
 //    org.kevoree.api.Port out;
 
-//    @Input
-//    public String in(Object i) {
+    @Input
+    public synchronized void onNewBackend(Object port) {
+        int newPort = Integer.parseInt(port.toString());
+        if (!ports.contains(newPort))
+            ports.add(newPort);
 //        String msg = message+" from "+context.getInstanceName()+"@"+context.getNodeName();
 //        System.out.println(msg);
 //        out.send(msg);
 //        return msg;
-//    }
+    }
 
     private HttpProxyServer server;
 
     @Start
     public void start() {
-//         server = DefaultHttpProxyServer.bootstrap()
-//                 .withPort(8080)
-//                 .withFiltersSource(new HttpFiltersSourceAdapter() {
-//                     public HttpFilters filterRequest(HttpRequest originalRequest, ChannelHandlerContext ctx) {
-//                         return new HttpFiltersAdapter(originalRequest) {
-//
-//                             @Override
-//                             public HttpResponse requestPre(HttpObject httpObject) {
-//                                 // TODO: implement your filtering here
-//                                 return null;
-//                             }
-//
-//                             @Override
-//                             public HttpResponse requestPost(HttpObject httpObject) {
-//                                 // TODO: implement your filtering here
-//                                 return null;
-//                             }
-//
-//                             @Override
-//                             public HttpObject responsePre(HttpObject httpObject) {
-//                                 // TODO: implement your filtering here
-//                                 return httpObject;
-//                             }
-//
-//                             @Override
-//                             public HttpObject responsePost(HttpObject httpObject) {
-//                                 // TODO: implement your filtering here
-//                                 return httpObject;
-//                             }
-//                         };
-//                     }
-//                 })
-//                 .start();
+        Log.info("Starting Proxy:{} at port {}", context.getInstanceName(), port);
+         server = DefaultHttpProxyServer.bootstrap()
+                 .withPort(port).withAllowLocalOnly(false)
+                 .withFiltersSource(new HttpFiltersSourceAdapter() {
+                     public HttpFilters filterRequest(HttpRequest originalRequest, ChannelHandlerContext ctx) {
+                         return new HttpFiltersAdapter(originalRequest) {
 
+                             @Override
+                             public HttpResponse clientToProxyRequest(HttpObject httpObject) {
+                                 if (httpObject instanceof HttpRequest) {
+                                     HttpRequest request = (HttpRequest) httpObject;
+                                     try {
+                                         URI uri = URI.create(request.getUri());
+                                         if (uri.getHost().equals("inti.magic.site")) {
+                                             printHttpRequest(request);
+                                             int portIndex = increaseAndGet();
+
+                                             Log.info("\t\t\t I have to translate the request and send it to localhost:{}", ports.get(portIndex));
+
+                                             URI newUri = new URI(uri.getScheme(), uri.getUserInfo(), "localhost",
+                                                     ports.get(portIndex), uri.getPath(), uri.getQuery(), uri.getFragment());
+                                             request.setUri(newUri.toString());
+                                         }
+                                         return null;
+                                     } catch (URISyntaxException e) {
+                                         e.printStackTrace();
+                                     }
+                                 }
+                                 HttpResponse response = new DefaultHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.BAD_GATEWAY);
+                                 return response;
+                             }
+                         };
+
+                     }
+                 }).start();
+
+                 }
+
+    private void printHttpRequest(HttpRequest request) {
+        String s = "";
+        for (Map.Entry<String, String> entry : request.headers().entries()) {
+            s += String.format("\t\t%s -> %s\n", entry.getKey(), entry.getValue());
+        }
+        Log.debug("A messsage is being sent: \n" +
+                        "uri={}\n" +
+                        "method={}\n" +
+                        "protocolVersion={}\n" +
+                        "headers=\n{}\n",
+                request.getUri(),
+                request.getMethod(),
+                request.getProtocolVersion(),
+                s);
     }
 
     @Stop
--- http_proxy_component/src/main/java/org/kevoree/microsandbox/http_proxy_component/WebServerExecuter.java
+++ http_proxy_component/src/main/java/org/kevoree/microsandbox/http_proxy_component/WebServerExecuter.java
@@ -1,30 +1,127 @@
 package org.kevoree.microsandbox.http_proxy_component;
 
 import org.kevoree.annotation.*;
+import org.kevoree.log.Log;
+
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.Socket;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 @ComponentType
-@Library(name = "Java")
 public class WebServerExecuter {
 
-    @Param(defaultValue = "Default Content")
-    String message;
+    @Param(optional = false)
+    String path_to_sosie;
+
+    @Param(optional = false)
+    String path_to_mdms;
+
+    @Param(optional = false)
+    int port;
 
     @KevoreeInject
     org.kevoree.api.Context context;
 
     @Output
-    org.kevoree.api.Port out;
+    org.kevoree.api.Port registerBackend;
 
-    @Input
-    public String in(Object i) {
-        String msg = message+" from "+context.getInstanceName()+"@"+context.getNodeName();
-        System.out.println(msg);
-        out.send(msg);
-        return msg;
+    class ServerExecuter implements Runnable {
+
+        private ClassLoader loader;
+
+        public ServerExecuter(ClassLoader loader) {
+            this.loader = loader;
+        }
+
+        @Override
+        public void run() {
+            long timeBefore = 0;
+            try {
+                Class<?> cl = loader.loadClass("org.ringojs.tools.launcher.Main");
+                Method method = cl.getMethod("main", new Class[]{String[].class});
+
+                String[] args = new String[] {
+                        path_to_mdms,
+                        String.format("--port=%d",port)
+                };
+                timeBefore = System.nanoTime();
+                method.invoke(null, new Object[]{args});
+            }catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (InvocationTargetException e) {
+                e.printStackTrace();
+                System.err.println("The invoked method throw an exception: not a big deal if the test has finished");
+            }
+            catch (Exception e) {
+                e.printStackTrace();
+
+            }
+            finally {
+//                System.setSecurityManager(null);
+//                long consumedTime = System.nanoTime() - timeBefore;
+//                System.out.println("============================================");
+//                System.out.printf(" Execution Time: %f seconds\n", consumedTime/1000000000.0);
+//                System.out.println("============================================");
+//                try {
+//                    Socket socket = new Socket("localhost",4444);
+//                    socket.getOutputStream().write(Double.toString(consumedTime/1000000000.0).getBytes());
+//                    socket.close();
+//                } catch (IOException e) { }
+//                System.exit(0);
+            }
+        }
+    }
+
+    private class DelayStarter implements Runnable {
+
+        @Override
+        public void run() {
+            int c = 0;
+            try {
+//                Thread.sleep(200);
+                String jar_path = path_to_sosie + "/run.jar";
+                ClassLoader loader = new URLClassLoader(new URL[]{new File(jar_path).toURI().toURL()}, Thread.currentThread().getContextClassLoader());
+                Thread th = new Thread(new ServerExecuter(loader));
+                th.setContextClassLoader(loader);
+                th.start();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
     @Start
-    public void start() {}
+    public void start() {
+        // let's start the backend
+        new Thread(new DelayStarter()).start();
+        // let's register the backend
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(3000);
+                    while (registerBackend.getConnectedBindingsSize() == 0) {
+                        Thread.sleep(100);
+                    }
+                    if (registerBackend.getConnectedBindingsSize() > 0) {
+                        registerBackend.send(port);
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
 
     @Stop
     public void stop() {}
